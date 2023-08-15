05b43bba9915986b6bd317d7196b0f2e87197b6c
Tch1b0 <Jpour3006@gmail.com>
2023-08-15 16:08:25 +0200

    add radio display

--- levels/Lab.gd
+++ levels/Lab.gd
@@ -3,5 +3,4 @@ extends Node2D
 var radio_display_scene := preload("res://components/displays/radio/RadioDisplay.tscn")
 
 func _ready() -> void:
-	var radio_display = radio_display_scene.instantiate()
-	$Sprite2D/RadioScreen.child_scene = radio_display
+	$Sprite2D/RadioScreen.child_scene = radio_display_scene.instantiate()
