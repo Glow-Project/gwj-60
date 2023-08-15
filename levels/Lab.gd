081db1d96eef82328bdf77bd4ec23c4faa07dab3
Tch1b0 <Jpour3006@gmail.com>
2023-08-15 17:58:40 +0200

    add robot

--- levels/Lab.gd
+++ levels/Lab.gd
@@ -3,4 +3,9 @@ extends Node2D
 var radio_display_scene := preload("res://components/displays/radio/RadioDisplay.tscn")
 
 func _ready() -> void:
-	$Sprite2D/RadioScreen.child_scene = radio_display_scene.instantiate()
+	boot_radio_screen()
+
+func boot_radio_screen() -> void:
+	var radio_display = radio_display_scene.instantiate()
+	radio_display.connect("radio_update", func(on: bool): $Radio.playing = on)
+	$RadioScreen.child_scene = radio_display
