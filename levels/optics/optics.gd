bdd1ba6d135769129639db737d55aa9fb1a4bb1c
Raphael Pour <info@raphaelpour.de>
2023-08-17 19:59:55 +0200

    more

--- levels/optics/optics.gd
+++ levels/optics/optics.gd
@@ -1,27 +1,30 @@
 extends Node2D
 
-@onready var ray = $Ray
-@onready var caster = $RayCast2D
-@onready var collision1 = $StaticBody2D/CollisionShape2D
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ray.clear_points()
-	ray.add_point($Emitter.global_position)
+	$Ray.clear_points()
+	$Ray.add_point($Emitter.global_position)
 	
-	caster.global_position = $Emitter.global_position
-	caster.target_position = Vector2(100,0)
-	$RayCast2D.force_raycast_update()
+	$RayCast.global_position = $Emitter.global_position
+	$RayCast.target_position = Vector2(100,0)
 	
-	ray.add_point($RayCast2D.get_collision_point())
+	var reflect_target: Vector2
+	for n in 5:
+		$RayCast.force_raycast_update()
+		if !$RayCast.is_colliding():
+			return
+		
+		$Ray.add_point($RayCast.get_collision_point())
+		
+		reflect_target = (
+			$RayCast.get_collision_point() - $RayCast.global_position
+			).bounce($RayCast.get_collision_normal())
+		$RayCast.global_position = $RayCast.get_collision_point() + reflect_target.normalized()*2
+		$RayCast.target_position = reflect_target.normalized()*100
 	
-	var reflect_target = ($RayCast2D.get_collision_point() - $RayCast2D.global_position).bounce($RayCast2D.get_collision_normal())
-	$RayCast2D.global_position = $RayCast2D.get_collision_point()
-	$RayCast2D.target_position = reflect_target*100
-	ray.add_point($RayCast2D.global_position)
-	
-	$StaticBody2D/CollisionShape2D.rotate(0.01)
+	print($Ray.points)
+	#$StaticBody2D/CollisionShape2D.rotate(0.01)
